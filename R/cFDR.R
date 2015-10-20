8046d54abab35501095e9c5ae0ef9853e9eab37c
kehaowu <kehao.wu@gmail.com>
2015-10-20 11:04:51 -0500

    update

--- R/cFDR.R
+++ R/cFDR.R
@@ -1,21 +1,56 @@
-cFDR = function(p_i,p_j) {
-
+cFDR = function(p1,p2) {
+  p_vals_ij = data.frame(p1,p2)
+  f_i = rep(0,dim(p_vals_ij)[1])
+  
+  if (F) {
+    # Compute cutoff c_i
+    p_i = p_vals_ij[,1]; p_j = p_vals_ij[,2];
+    ord=order(p_i); cdf=(1:length(ord))/length(ord) # Quantiles of ordered list of p values
+    p_i = p_i[ord];
+    fdr1 = p_i/cdf;
+    c_i = p_i[min(which(fdr1>0.1))]
+    
+    r1 = which(log(p_vals_ij[,1])+log(p_vals_ij[,2])<0.5*log(c_i)) # values not in triangle (p_i,p_j<1; -log10(p_i) + -log10(p_j) <-log10(c_i))
+    r2 = which(p_vals_ij[,1]<(10^-0.75)) # 
+    #r3 = which(p_vals_ij[,1]>(10^-12)) # SNPs with values lower than this 
+    r = intersect(r1,r2) # intersect(intersect(r1,r2),r3) # Values with ambiguous FDR
+    
+    total = 1:length(f_i);
+    f_i[setdiff(total,r1)]=1; # approximately
+    f_i[setdiff(total,r2)]=1; # approximately
+    #f_i[setdiff(total,r3)]=0;
+    
+  } else {
+    r = 1:dim(p_vals_ij)[1]
+  }
+  
+  p_i = p_vals_ij[,1]; # Remaining points
+  p_j = p_vals_ij[,2];
+  
+  
   # Compute empirical FDR at remaining points
   
   cdf = rep(0,length(p_i))
   
   oj = order(p_j)
   p_i1 = p_i[oj]; p_j1 = p_j[oj] #
-  or = order(oj) # index of snps in R in p_i1/p_j1
-
-  pb = txtProgressBar(min = 1,max = length(p_i1),style=3)
-  for (i in 1:length(p_i1)) {
-    cdf[i] = length(which(p_i1[1:or[i]]<=p_i1[or[i]]))/or[i]
-    setTxtProgressBar(pb = pb,value = i)
+  or = order(oj)[r] # index of snps in R in p_i1/p_j1
+  cr = rep(0,length(r))
+  bp = txtProgressBar(min = 0,max = length(r))
+  cat("oj",oj,"\n")
+  
+  for (i in 1:length(r)) {
+    cdf[r[i]] = length(which(p_i1[1:or[i]]<=p_i1[or[i]]))/or[i]
+    cat(cdf[r[i]],"\n")
   }
   
+  # Equivalent technique
   
-  f_i=p_i1/cdf
+  #for (i in 1:length(r)) {
+  # cdf[r[i]] = length(which(p_i<=p_i[r[i]] & p_j<=p_j[r[i]]))/length(which(p_j<=p_j[r[i]]))
+  #}
+  
+  f_i[r]=p1[r]/cdf[r] # Computation of FDR
   
   return(f_i)
 }
